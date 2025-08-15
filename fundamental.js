2c62e590c31a94bd0e5708fd169898bb6dcb3723
Md Bocktear Abid Borat <boratabid@gmail.com>
2025-08-15 19:09:59 +0600

    commit-3

---
+++ fundamental.js
@@ -0,0 +1,19 @@
+// how to declare a variable using let and const
+const fatername = 'mostofa';
+let season = 'rainy';
+season = 'winter';
+
+//  6 basic conditions >,<, ===, !==, <=, >=
+
+// es6 
+ const numbers = [89,35,98,12];
+ const  student = {
+    name: 'abid khan',
+    age: 25,
+    movies:['king', 'dhakar mastan']
+ };
+
+ const about = `My Name is ${student.name} age of  ${student.age} has number ${numbers[3]}
+  also like movies ${student.movies}`;
+
+ console.log(about)
